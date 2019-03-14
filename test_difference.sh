70e221b58157b731091e21e11b21820f1e5ccf49
NegatioN <joakimrishaug@gmail.com>
2019-03-14 11:30:31 +0100

    Add a bit more logging

--- test_difference.sh
+++ test_difference.sh
@@ -2,5 +2,7 @@
 
 docker build . --tag minimal_caffe2
 
+echo "Pinned to one core:"
 docker run --rm -p 8081:8080 --cpus 0.5 --cpuset-cpus 0 --name minimal_caffe2 minimal_caffe2
+echo "Pinned to four cores:"
 docker run --rm -p 8081:8080 --cpus 0.5 --cpuset-cpus 0-3 --name minimal_caffe2 minimal_caffe2
